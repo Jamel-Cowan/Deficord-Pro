d6fb5aa440efd4a7300b0f8cda7c745020adb1de
Block Noob <98855710+Jamel-Cowan@users.noreply.github.com>
2023-08-12 14:04:50 -0400

    Deficord.js

--- test/Deficord.js
+++ test/Deficord.js
@@ -5,22 +5,30 @@ const tokens = (n) => {
 }
 
 describe("Deficord", function () {
+  let deficord
+
+  const NAME = "Deficord"
+  const SYMBOL = "DC"
+
+  beforeEach(async () => {
+    //Deploy Contract
+    const Deficord = await ethers.getContractFactory("Deficord")
+    deficord = await Deficord.deploy(NAME, SYMBOL)
+  })
 
   describe("Deployment", function () {
-    it("Sets the name & Symbol", async () => {
-      //Deploy Contract
-      const Deficord = await ethers.getContractFactory("Deficord")
-      deficord = await Deficord.deploy("Deficord", "DC")
-
+    it("Sets the name", async () => {
       // Fetch name
       let result = await deficord.name()
       // Check name 
-      expect(result).to.equal("Deficord")
-      
+      expect(result).to.equal(NAME)
+    })
+
+    it("Sets the Symbol", async () => {
       // Fetch symbol
-      result = await deficord.symbol()
+      let result = await deficord.symbol()
       // Check symbol
-      expect(result).to.equal("DC")
+      expect(result).to.equal(SYMBOL)
     })
   })
 })
