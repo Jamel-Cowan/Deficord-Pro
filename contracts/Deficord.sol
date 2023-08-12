0414d2699ddd020ae478f6f7b1d0b24e2ce34cd5
Block Noob <98855710+Jamel-Cowan@users.noreply.github.com>
2023-08-12 13:52:08 -0400

    Deficord.js

--- contracts/Deficord.sol
+++ contracts/Deficord.sol
@@ -1,4 +1,13 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
-contract Deficord {}
+contract Deficord is ERC721 {
+
+constructor(string memory _name, string memory _symbol) 
+    ERC721(_name, _symbol)
+{
+
+}
+
+}
--- test/Deficord.js
+++ test/Deficord.js
@@ -6,4 +6,21 @@ const tokens = (n) => {
 
 describe("Deficord", function () {
 
+  describe("Deployment", function () {
+    it("Sets the name & Symbol", async () => {
+      //Deploy Contract
+      const Deficord = await ethers.getContractFactory("Deficord")
+      deficord = await Deficord.deploy("Deficord", "DC")
+
+      // Fetch name
+      let result = await deficord.name()
+      // Check name 
+      expect(result).to.equal("Deficord")
+      
+      // Fetch symbol
+      result = await deficord.symbol()
+      // Check symbol
+      expect(result).to.equal("DC")
+    })
+  })
 })
