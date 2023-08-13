47303e98888983bb9e1874712f29e0f995061b30
Block Noob <98855710+Jamel-Cowan@users.noreply.github.com>
2023-08-12 22:34:43 -0400

    Navigation.js

--- src/App.js
+++ src/App.js
@@ -19,9 +19,17 @@ const socket = io('ws://localhost:3030');
 
 function App() {
 
+  const loadBlockchainData = async () => {
+    console.log('loading...')
+  }
+
+  useEffect(() => {
+    loadBlockchainData()
+  }, [] )
+
   return (
     <div>
-      <h1 style={{ textAlign: "center", padding: "15px" }}>Welcome to Deficord</h1>
+      <Navigation />
 
       <main>
 
