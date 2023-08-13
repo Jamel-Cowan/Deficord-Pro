7d3ed24b9732e5fe9f1915a95464d42b74ebb393
Block Noob <98855710+Jamel-Cowan@users.noreply.github.com>
2023-08-13 11:58:28 -0400

    Channels.js

--- src/components/Channels.js
+++ src/components/Channels.js
@@ -1,9 +1,32 @@
 const Channels = ({ provider, account, deficord, channels, currentChannel, setCurrentChannel }) => {
 
+  const channelHandler = async (channel) => {
+    const hasJoined = await deficord.hasJoined(channel.id, account)
+
+    if(hasJoined) {
+      setCurrentChannel(channel)
+    } else {
+     const signer = await provider.getSigner()
+     const transaction = await deficord.connect(signer).mint(channel.id, { value: channel.cost })
+     await transaction.wait()
+     setCurrentChannel(channel)
+    }
+  }
+
   return (
     <div className="channels">
       <div className="channels__text">
-        <h2>Text Channels</h2>
+       <h2>Text Channels</h2>
+      
+    <ul>
+    {channels.map((channel, index)=> (
+       <li 
+       key={index}
+       onClick={() => channelHandler(channel)}
+      className={currentChannel && currentChannel.id.toString() === channel.id.toString() ? "active" : ""}>
+      {channel.name}</li>
+      ))}
+     </ul>
 
       </div>
 
