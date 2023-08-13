2d81e7818a779dcb6950667c9d333487d2d30507
Block Noob <98855710+Jamel-Cowan@users.noreply.github.com>
2023-08-12 22:05:07 -0400

    deploy.js

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -5,7 +5,29 @@ const tokens = (n) => {
 }
 
 async function main() {
+  //Setup accounts & variables
+  const [deployer] = await ethers.getSigners()
+  const NAME = "Deficord"
+  const SYMBOL = "DC"
+  // Deployment goes here
+  const Deficord = await ethers.getContractFactory("Deficord")
+  const deficord = await Deficord.deploy(NAME, SYMBOL)
+  await deficord.deployed()
 
+  console.log(`Deployed Deficord Contract at: ${deficord.address}\n`)
+
+  // Create 3 channels
+  const CHANNEL_NAMES = ["general", "intro", "jobs"]
+  const COSTS = [tokens(1), tokens(0), tokens(0.25)]
+
+  for (var i = 0; i < 3; i++){
+  // Code goes here... channel creation
+    const transaction = await deficord.connect(deployer).createChannel(CHANNEL_NAMES[i], COSTS[i])
+    await transaction.wait()
+
+    console.log(`Created text channel #${CHANNEL_NAMES[i]}`)
+
+  }
 }
 
 main().catch((error) => {
